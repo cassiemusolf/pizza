0279c3445c57a9fc91a9eebed86fafed7fb1db7d
Cassie Musolf <cassiemusolf@gmail.com>
2017-01-27 09:06:21 -0800

    add form to HTML to hold radio buttons for sizes and checkboxes for toppings

---
+++ js/scripts.js
@@ -0,0 +1,25 @@
+//business logic
+
+function Pizza(size, toppings) {
+  this.size = size;
+  this.toppings = toppings;
+}
+
+Pizza.prototype.price = function() {
+
+}
+
+
+
+
+
+
+//front-end logic
+
+$(document).ready(function(){
+  var pizzaOne = new Pizza();
+  var pizzaTwo = new Pizza();
+});
+
+var inputtedPizzaSize = $ ("input#")
+var inputtedPizzaToppings = $
