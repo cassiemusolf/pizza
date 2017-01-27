64561cddd030a9662b73253f011bdddc24a9a520
Cassie Musolf <cassiemusolf@gmail.com>
2017-01-27 09:27:12 -0800

    add checkboxes for pizza toppings

--- js/scripts.js
+++ js/scripts.js
@@ -17,9 +17,13 @@ Pizza.prototype.price = function() {
 //front-end logic
 
 $(document).ready(function(){
+  $("form#createPizza").submit(function(event){
+    event.preventDefault();
   var pizzaOne = new Pizza();
   var pizzaTwo = new Pizza();
-});
 
 var inputtedPizzaSize = $ ("input#")
 var inputtedPizzaToppings = $
+
+})
+});
